8672494d51c46ba39aa0720036f8f5425708716d
Blake Wang <blakecwang@gmail.com>
2015-03-11 20:07:11 -0700

    put map initialize function into app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 /*
 	API key: AIzaSyBWMekTXMrGU0DNq4CiFH5ZcFW8L_Wsm1A
 */
+
+function initialize() {
+var mapOptions = {
+  center: { lat: 32.759905, lng: -117.127455},
+  zoom: 16
+};
+var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
