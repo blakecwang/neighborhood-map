9a8cf23161a7c7fc4ac6c804b8f245ea32ebd1e6
Blake Wang <blakecwang@gmail.com>
2015-03-11 20:09:38 -0700

    put maps src script tag in body instead of head

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@
 */
 
 function initialize() {
-var mapOptions = {
-  center: { lat: 32.759905, lng: -117.127455},
-  zoom: 16
-};
-var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	var mapOptions = {
+	  center: { lat: 32.759905, lng: -117.127455},
+	  zoom: 16
+	};
+	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
