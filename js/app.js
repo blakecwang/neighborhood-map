cb39cd7fe14e4343dcb41fedb24aa0a9f09c83ce
Blake Wang <blakecwang@gmail.com>
2015-03-11 20:50:06 -0700

    create a places object

--- js/app.js
+++ js/app.js
@@ -2,6 +2,44 @@
 	API key: AIzaSyBWMekTXMrGU0DNq4CiFH5ZcFW8L_Wsm1A
 */
 
+
+//----------- MODEL -----------//
+var model = function() {
+
+	var home = {
+		'name': 'Blake\'s House',
+		'lat': 32.759904, 
+		'lng': -117.127454
+	};
+
+	var places = [
+		{
+			'name': home.name,
+			'lat': home.lat, 
+			'lng': home.lng
+		},
+		{
+			'name': 'Coin-Op Game Room',
+			'lat': 32.7490327, 
+			'lng': -117.1303769
+		},
+		{
+			'name': 'Soda Bar',
+			'lat': 32.7398671,
+			'lng': -117.1205925
+		},
+		{
+			'name': 'Seven Grand',
+			'lat': 32.7486272, 
+			'lng': -117.1286325
+		},
+			'name': 'Rigoberto\'s Taco Shop',
+			'lat': 32.7216101, 
+			'lng': -117.0858673
+		}
+	];
+};
+
 function initialize() {
 	var mapOptions = {
 	  center: { lat: 32.759905, lng: -117.127455},
