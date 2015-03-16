28b49c3927f723a59a4e9c429192c6c43e331215
Blake Wang <blakecwang@gmail.com>
2015-03-15 17:01:06 -0700

    re-org and iterate through places array to add markers

--- js/app.js
+++ js/app.js
@@ -4,57 +4,58 @@
 
 
 var home = {
-		'name': 'Blake\'s House',
-		'lat': 32.759904, 
-		'lng': -117.127454
-	};
-
-//----------- MODEL -----------//
-var model = function() {
-
-	
-
-	var places = [
-		home,
-		{
-			'name': 'Coin-Op Game Room',
-			'lat': 32.7490327, 
-			'lng': -117.1303769
-		},
-		{
-			'name': 'Soda Bar',
-			'lat': 32.7398671,
-			'lng': -117.1205925
-		},
-		{
-			'name': 'Seven Grand',
-			'lat': 32.7486272, 
-			'lng': -117.1286325
-		},
-		{
-			'name': 'Rigoberto\'s Taco Shop',
-			'lat': 32.7216101, 
-			'lng': -117.0858673
-		}
-	];
+	'name': 'Blake\'s House',
+	'lat': 32.759904, 
+	'lng': -117.127454
 };
+
+
+var places = [
+	home,
+	{
+		'name': 'Coin-Op Game Room',
+		'lat': 32.7490327, 
+		'lng': -117.1303769
+	},
+	{
+		'name': 'Soda Bar',
+		'lat': 32.7398671,
+		'lng': -117.1205925
+	},
+	{
+		'name': 'Seven Grand',
+		'lat': 32.7486272, 
+		'lng': -117.1286325
+	},
+	{
+		'name': 'Rigoberto\'s Taco Shop',
+		'lat': 32.7216101, 
+		'lng': -117.0858673
+	}
+];
+
 
 function init() {
 
-	var mapOptions = {
-	  center: { lat: home.lat, lng: home.lng},
-	  zoom: 16
-	};
-	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	// init map
+	var map = new google.maps.Map( document.getElementById( 'map-canvas' ),
+		{
+			center: { lat: home.lat, lng: home.lng },
+		 	zoom: 16
+		}
+	);
 
-	var latLng = new google.maps.LatLng(home.lat, home.lng);
-	var markerOptions = {
-		position: latLng,
-		map: map,
-		title: home.name
-	};
-	var marker = new google.maps.Marker(markerOptions);
-	
+	// init markers
+	for ( var i = 0; i < home.length; i++) {
+		var latLng = new google.maps.LatLng( places[i].lat, places[i].lng );
+		var marker = new google.maps.Marker(
+			{
+				position: latLng,
+				map: map,
+				title: places[i].name
+			}
+		);
+	}
 }
 google.maps.event.addDomListener(window, 'load', init);
 
